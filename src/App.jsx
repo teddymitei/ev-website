7f4eedb23b850e568dad6fdd63d3870c4d35555d
Maverick-ke <106772779+Maverick-ke@users.noreply.github.com>
2024-06-10 14:13:09 +0300

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react" 
 import Background from "./Components/Background/Background";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 const App = () => {
   let heroData = [
@@ -14,6 +15,13 @@ const App = () => {
     <div>
       <Background playStatus={playStatus} heroCount={heroCount}/>
       <Navbar/>
+      <Hero
+      setPlayStatus={setPlayStatus}
+      heroData={heroData[heroCount]}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+      />
     </div>
   )
 }
