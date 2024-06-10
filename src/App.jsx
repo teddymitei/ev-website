d381273445a5cac537e40c1afef2085e87c64fba
Maverick-ke <106772779+Maverick-ke@users.noreply.github.com>
2024-06-10 13:08:56 +0300

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import { useState } from "react" 
+import Background from "./Components/Background/Background";
 
 const App = () => {
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give into",text2:"your passions"},
+  ]
+  const [heroCount,setHeroCount] = useState(0);
+  const [playStatus,setPlayStatus]= useState(false);
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   )
 }
